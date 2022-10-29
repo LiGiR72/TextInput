3819a3a69919cf01729a73860ca61182f0c23a4e
LiGiR72 <iliya.alabyew@gmail.com>
2022-10-29 16:27:08 +0300

    first task completed

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,25 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File in = new File("src/basket.txt");
         String[] groceries = {"Молоко", "Хлеб", "Сыр", "Гречка", "Рис"};
-        int[] prices = {70, 30, 100, 35, 40};
-        int[] shoppingList = new int[5];
-        Scanner scanner = new Scanner(System.in);
-        showPrices(groceries, prices);
-        while (true) {
-            System.out.println("Введите:\nНомер товара и его количество, чтобы добваить в корзину");
-            System.out.println("Введите end, чтобы выйти из программы");
-            System.out.println("Список, чтобы вывести цены еще раз");
-            System.out.println("Сумма, чтобы высчитать итоговую сумму покупок");
-            System.out.print(">> ");
-            String input = scanner.nextLine();
-            if (input.equals("end")) {
-                break;
-            }
-            if (input.equals("Список")) {
-                showPrices(groceries, prices);
-                continue;
-            }
-            if (input.equals("Сумма")) {
-                int sum = 0;
-                for (int i = 0; i < shoppingList.length; i++) {
-                    if (shoppingList[i] == 0)
-                        continue;
-                    System.out.println(groceries[i] + " " + prices[i] + " р/шт " + (shoppingList[i] / prices[i]) + " штук  = " + shoppingList[i] + " рублей");
-                    sum += shoppingList[i];
-                }
-                System.out.println("Итого = " + sum + " рублей");
-                continue;
-            }
-            String[] product = input.split(" ");
-            int productPos = Integer.parseInt(product[0]) - 1;
-            int productCount = Integer.parseInt(product[1]);
-            shoppingList[productPos] += prices[productPos] * productCount;
+        Basket basket;
+        long[] prices = {70, 30, 100, 35, 40};
+        if (!in.exists()) {
+            in.createNewFile();
+            basket = new Basket(prices, groceries);
+        } else {
+            basket = Basket.loadFromTxt(in);
         }
-    }
+        basket.printCart();
+        basket.addToCart(2, 10);
+        basket.addToCart(5, 5);
+        basket.printCart();
 
-    public static void showPrices(String[] grc, int[] prices) {
-        for (int i = 0; i < grc.length; i++) {
-            System.out.println((i + 1) + " " + grc[i] + " " + prices[i] + " р/шт");
-        }
     }
 }
+
+
